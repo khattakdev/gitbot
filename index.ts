6eba30ffdad78390fd87a9ead9677882c7239693
Arsalan Khattak <akkhattak65@gmail.com>
2020-12-19 17:20:34 +0500

    feat: folder checker
    Check the folder and verify if it's a valid folder to start the application.

--- index.ts
+++ index.ts
@@ -1,15 +1,1 @@
-import fs = require('fs');
-import path = require('path');
-import process = require('process');
-const { argv } = process;
-getli();
-
-function getli() {
-    const currentDir = path.resolve(__dirname, argv[2] || '');
-    fs.readdir(currentDir, (err, files) => {
-        if (files.length > 0) {
-            console.log('Please use an empty Folder');
-            process.exit(0);
-        }
-    });
-}
+require('./package/init')();
